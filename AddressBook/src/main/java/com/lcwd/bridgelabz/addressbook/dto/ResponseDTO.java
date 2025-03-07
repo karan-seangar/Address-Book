d278268eacee4266cc1a25fcf5859ed86e32aad3
karan-seangar <seangarkaran@gmail.com>
2025-03-07 11:58:00 +0530

    [karan seangar] added dto and model in address Book

---
+++ AddressBook/src/main/java/com/lcwd/bridgelabz/addressbook/dto/ResponseDTO.java
@@ -0,0 +1,14 @@
+package com.lcwd.bridgelabz.addressbook.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDTO {
+    private String message;
+    private Object data;
+}
