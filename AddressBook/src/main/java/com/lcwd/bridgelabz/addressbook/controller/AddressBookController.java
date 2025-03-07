dffa3bd446578b94f43038fb5507c7527121afeb
karan-seangar <seangarkaran@gmail.com>
2025-03-07 11:44:37 +0530

    [karan seangar] added all controllers of address book

--- AddressBook/src/main/java/com/lcwd/bridgelabz/addressbook/controller/AddressBookController.java
+++ AddressBook/src/main/java/com/lcwd/bridgelabz/addressbook/controller/AddressBookController.java
@@ -1,9 +1,7 @@
 package com.lcwd.bridgelabz.addressbook.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/address")
@@ -13,4 +11,29 @@ public class AddressBookController {
     public ResponseEntity<String> test(){
        return ResponseEntity.ok("Hello From Address Book Controller");
    }
+
+   @GetMapping({ "","/","/get"})
+    public ResponseEntity<String> getAll(){
+       return ResponseEntity.ok("All contacts displayed");
+   }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<String> find(@PathVariable Long id) {
+        return ResponseEntity.ok("Contact with ID " + id + " displayed");
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> add(@RequestBody String contactDetails) {
+        return ResponseEntity.ok("Contact added: " + contactDetails);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> update(@PathVariable Long id, @RequestBody String updatedDetails) {
+        return ResponseEntity.ok("Contact with ID " + id + " updated: " + updatedDetails);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        return ResponseEntity.ok("Contact with ID " + id + " deleted");
+    }
+
 }
